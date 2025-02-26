4b4d7d4e6d8a2f15a327c90f332b4c04854dec41
marcophilip <marcophilipagapito@gmail.com>
2025-02-25 20:00:14 -0800

    project in progress

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// create the consts for all the id and classes
+const elQuestion = document.querySelector("#question");
+const elChoices = document.querySelector("#choices");
+const elResult = document.querySelector("#result");
+const elNext = document.querySelector("#next-btn");
+const elSubmit = document.querySelector("#submit-btn");
+const elStart = document.querySelector("#start-btn");
+const elScore = document.querySelector("#score");
+//create the variables for scores
+let score = 0;
+let currentQuestIdx = 0;
+let correct = false;
+
+
+
+
+
+//create a function when starting the quiz
+
+// create a function that will show the question 
+
+//create a function that will show the choices 
+
+//create a function that will capture the choice when clicked
+
+//create a function that will show the result for each question 
+
+//create a function that will compare the player choice and correct answer
+
+
+
+// add an event listener for each buttons when clicked
---
+++ js/data.js
@@ -0,0 +1,105 @@
+//create an array of objects for all the datas.
+// start with questionaires. each index includes the question, choices and the answer.
+
+const questions = [
+    {
+        question: "Which team is the 1996 NBA Champions?",
+        choices: ["Utah Jazz", "Houston Rockets", "Chicago Bulls", "Miami Heat"],
+        answer: "Chicago Bulls",
+    },
+    {
+        question: "Which Men's Basketball Team won the first Gold Medal in 1936 Berlin Olympics?",
+        choices: ["Yugoslavia", "Philippines", "Spain", "United States"],
+        answer: "United States",
+    },
+    {
+        question: "Who scored the most points in Olympics basketball?",
+        choices: ["Oscar Schmidt", "Carmelo Anthony", "Pau Gasol", "Andrew Gaze"],
+        answer: "Oscar Schmidt",
+    },
+    {
+        question: "Which country won the men's basketball gold medal match against France in 2004",
+        choices: ["United States", "Argentina", "China", "Croatia"],
+        answer: "Argentina",
+    },
+    {
+        question: "Who is the NBA all-time triple-double leader?",
+        choices: ["Nikola Jokić", "Magic Johnson", "Oscar Robertson", "Russel Westbrook"],
+        answer: "Russel Westbrook",
+    },
+    {
+        question: "Who won the Season, All-Star and Finals MVP in the same NBA season?",
+        choices: ["LeBron James", "Kareem-Abdul Jabbar", "Michael Jordan", "Kobe Bryant"],
+        answer: "Michael Jordan",
+    },
+    {
+        question: "Who is the youngest NBA Season MVP?",
+        choices: ["LeBron James", "Magic Johnson", "Karl Malone", "Derrick Rose"],
+        answer: "Derrick Rose",
+    },
+    {
+        question: "Which NBA team holds the best Win-Lose record in a season?",
+        choices: ["Cleveland Cavaliers", "Chicago Bulls", "Boston Celtics", "Golden State Warriors"],
+        answer: "Golden State Warriors",
+    },
+    {
+        question: "Who is the first pick during the 1996 NBA Draft?",
+        choices: ["Kobe Bryant", "Allen Iverson", "Steve Nash", "Peja Stojaković"],
+        answer: "Allen Iverson",
+    },
+    {
+        question: "Which player scored 13 points in 33 seconds in an NBA game?",
+        choices: ["Kevin Durant", "Tracy McGrady", "Kobe Bryant", "Shaquille O'neal"],
+        answer: "Tracy McGrady",
+    },
+    {
+        question: "",
+        choices: [],
+        answer: "",
+    },
+    {
+        question: "",
+        choices: [],
+        answer: "",
+    },
+    {
+        question: "",
+        choices: [],
+        answer: "",
+    },
+    {
+        question: "",
+        choices: [],
+        answer: "",
+    },
+    {
+        question: "",
+        choices: [],
+        answer: "",
+    },
+    {
+        question: "",
+        choices: [],
+        answer: "",
+    },
+    {
+        question: "",
+        choices: [],
+        answer: "",
+    },
+    {
+        question: "",
+        choices: [],
+        answer: "",
+    },
+    {
+        question: "",
+        choices: [],
+        answer: "",
+    },
+    {
+        question: "",
+        choices: [],
+        answer: "",
+    },
+]
