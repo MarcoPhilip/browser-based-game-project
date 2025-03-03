9a8dbb471e4ac863ba6eb51d0aead81ec736e68c
marcophilip <marcophilipagapito@gmail.com>
2025-03-02 16:09:46 -0800

    project in progress

--- js/app.js
+++ js/app.js
@@ -1,32 +1,152 @@
 // create the consts for all the id and classes
 const elQuestion = document.querySelector("#question");
-const elChoices = document.querySelector("#choices");
+const elChoices = document.querySelectorAll("#choices");
 const elResult = document.querySelector("#result");
 const elNext = document.querySelector("#next-btn");
 const elSubmit = document.querySelector("#submit-btn");
 const elStart = document.querySelector("#start-btn");
 const elScore = document.querySelector("#score");
+const elOptions = document.querySelector("#options");
+
+const buttonA = document.getElementById("a");
+const buttonB = document.getElementById("b");
+const buttonC = document.getElementById("c");
+const buttonD = document.getElementById("d");
+
 //create the variables for scores
 let score = 0;
 let currentQuestIdx = 0;
-let correct = false;
-
-
-
-
-
-//create a function when starting the quiz
-
-// create a function that will show the question 
-
-//create a function that will show the choices 
-
-//create a function that will capture the choice when clicked
-
-//create a function that will show the result for each question 
-
-//create a function that will compare the player choice and correct answer
-
-
+let answer = null;
 
 // add an event listener for each buttons when clicked
+elNext.addEventListener('click', nextQuestion);
+elStart.addEventListener('click', startGame);
+// elSubmit.addEventListener('click', gradeAnswer);
+buttonA.addEventListener('click', selectedAnswer);
+buttonB.addEventListener('click', selectedAnswer);
+buttonC.addEventListener('click', selectedAnswer);
+buttonD.addEventListener('click', selectedAnswer);
+
+
+
+//create a function when starting a quiz
+function startGame() {
+    // empty the score and current question idx
+    currentQuestIdx = 0;
+    score = 0;
+    answer = null
+    elResult.textContent = ""
+    // assign the functions for later where the questions and choices will populate
+    showQuestion();
+    showAnswers();
+};
+
+
+// create a function that will show the question 
+function showQuestion() {
+    //create a const and store the question[i]
+    let currentQuestion = questions[currentQuestIdx];
+    //create a const for numbering the questions
+    let questionNumber = currentQuestIdx + 1;
+    //combine the number and the question in the question display
+    elQuestion.textContent = questionNumber + ". " + currentQuestion.question; 
+};
+//create a function that will show the choices 
+function showAnswers() {
+    //display all the choices in each choices box
+    buttonA.textContent = questions[currentQuestIdx].answers[0].choice;
+    buttonB.textContent = questions[currentQuestIdx].answers[1].choice;
+    buttonC.textContent = questions[currentQuestIdx].answers[2].choice;
+    buttonD.textContent = questions[currentQuestIdx].answers[3].choice;
+};
+
+//create a function for selecting an answer with parameter
+function selectedAnswer(event) {
+    //the user answer is event.target.textContent
+    console.log(event.target.textContent);
+    //the var for the answers array
+    let choices = questions[currentQuestIdx].answers;
+    // console.log(choices)
+    //loop through the answers
+    for (let i = 0; i < choices.length; i++) {
+        //finding the correct answer from choices
+        if (choices[i].correct == true) {
+            //compare user answer to the choices[i].choice
+            if (event.target.textContent == choices[i].choice) {
+                // add a score
+                console.log(score)
+                score++;
+                console.log(score)
+                //display the result for the question
+                elResult.textContent = "That is the CORRECT answer!"
+                //
+            }
+            else {
+                //display an incorrect answer
+                elResult.textContent = "Incorrect answer!"
+            }
+            console.log(choices[i].choice)
+        }
+        console.log(choices[i].correct)
+    }
+}
+// console.log(questions[currentQuestIdx].answers)
+
+//create a function that will grade the answer
+
+// function gradeAnswer() {
+
+//    //if player did not select an answer, remind player to select an answer in the result display
+//     if (!answer) {
+//         //display the msg into the result display
+//         elResult.textContent = "Select an answer!";
+//         //return the output
+//         return;
+//     }
+//    //if the answer is correct
+//     if (answer.correct == true) {
+//         //add 1 value to the score
+//         score = score + 1;
+//         //display the result msg
+//         elResult.textContent = "That is the CORRECT answer!"
+//     }
+//     //if not correct, display the msg in the result display
+//     else {
+//         elResult.textContent = "Incorrect answer!"
+//     }
+// }
+
+//create a function for next question
+function nextQuestion() {
+    //move to the next question by adding one value to the current question idx
+    currentQuestIdx = currentQuestIdx + 1;
+    
+    //create an if statement so that the questions will stop after the last question
+    if (currentQuestIdx < questions.length) {
+        //call out the showQuestion and showAnswers function
+        showQuestion();
+        showAnswers();
+        //empty the result display
+        elResult.textContent = "";
+    }
+    // if reached the last question, end the quiz with a function
+    else {
+        endQuiz();
+    }
+
+    
+};
+
+//create a function for ending the quiz
+
+function endQuiz() {
+    //display a message in the question display that says quiz is over
+    elQuestion.textContent = "Congrats! You Have Finished The Quiz!"
+    //display the result of the quiz
+    elResult.textContent = `Your Score: ${score} out of ${questions.length}`
+    //hide the submit and next buttons
+    elSubmit.style.display = "none";
+    elNext.style.display = "none";
+};
+
+//create a function to reset
--- js/data.js
+++ js/data.js
@@ -1,105 +1,185 @@
 //create an array of objects for all the datas.
-// start with questionaires. each index includes the question, choices and the answer.
+// start with questionaires. follow with another object containing the choice and correct answer
 
 const questions = [
     {
         question: "Which team is the 1996 NBA Champions?",
-        choices: ["Utah Jazz", "Houston Rockets", "Chicago Bulls", "Miami Heat"],
-        answer: "Chicago Bulls",
+        answers: [
+            { choice: "Utah Jazz", correct: false },
+            { choice: "Houston Rockets", correct: false },
+            { choice: "Los Angeles Lakers", correct: false },
+            { choice: "Chicago Bulls", correct: true },
+        ]        
     },
     {
         question: "Which Men's Basketball Team won the first Gold Medal in 1936 Berlin Olympics?",
-        choices: ["Yugoslavia", "Philippines", "Spain", "United States"],
-        answer: "United States",
+        answers: [
+            { choice: "Yugoslavia", correct: false },
+            { choice: "Philippines", correct: false },
+            { choice: "Spain", correct: false },
+            { choice: "United States", correct: true },
+        ]        
     },
     {
         question: "Who scored the most points in Olympics basketball?",
-        choices: ["Oscar Schmidt", "Carmelo Anthony", "Pau Gasol", "Andrew Gaze"],
-        answer: "Oscar Schmidt",
+        answers: [
+            { choice: "Oscar Schmidt", correct: true },
+            { choice: "Carmelo Anthony", correct: false },
+            { choice: "Pau Gasol", correct: false },
+            { choice: "Andrew Gaze", correct: false },
+        ]
     },
     {
-        question: "Which country won the men's basketball gold medal match against France in 2004",
-        choices: ["United States", "Argentina", "China", "Croatia"],
-        answer: "Argentina",
+        question: "Which country won the men's basketball gold medal match against France in 2004?",
+        answers: [
+            { choice: "United States", correct: false },
+            { choice: "Argentina", correct: true },
+            { choice: "China", correct: false },
+            { choice: "Croatia", correct: false },
+        ]
     },
     {
         question: "Who is the NBA all-time triple-double leader?",
-        choices: ["Nikola Jokić", "Magic Johnson", "Oscar Robertson", "Russel Westbrook"],
-        answer: "Russel Westbrook",
+        answers: [
+            { choice: "Nikola Jokić", correct: false },
+            { choice: "Magic Johnson", correct: false },
+            { choice: "Oscar Robertson", correct: false },
+            { choice: "Russel Westbrook", correct: true },
+        ]
     },
     {
         question: "Who won the Season, All-Star and Finals MVP in the same NBA season?",
-        choices: ["LeBron James", "Kareem-Abdul Jabbar", "Michael Jordan", "Kobe Bryant"],
-        answer: "Michael Jordan",
+        answers: [
+            { choice: "LeBron James", correct: false },
+            { choice: "Kareem-Abdul Jabbar", correct: false },
+            { choice: "Michael Jordan", correct: true },
+            { choice: "Kobe Bryant", correct: false },
+        ]
     },
     {
         question: "Who is the youngest NBA Season MVP?",
-        choices: ["LeBron James", "Magic Johnson", "Karl Malone", "Derrick Rose"],
-        answer: "Derrick Rose",
+        answers: [
+            { choice: "LeBron James", correct: false },
+            { choice: "Magic Johnson", correct: false },
+            { choice: "Karl Malone", correct: false },
+            { choice: "Derrick Rose", correct: true },
+        ]
     },
     {
         question: "Which NBA team holds the best Win-Lose record in a season?",
-        choices: ["Cleveland Cavaliers", "Chicago Bulls", "Boston Celtics", "Golden State Warriors"],
-        answer: "Golden State Warriors",
+        answers: [
+            { choice: "Cleveland Cavaliers", correct: false },
+            { choice: "Chicago Bulls", correct: false },
+            { choice: "Boston Celtics", correct: false },
+            { choice: "Golden State Warriors", correct: true },
+        ]
     },
     {
         question: "Who is the first pick during the 1996 NBA Draft?",
-        choices: ["Kobe Bryant", "Allen Iverson", "Steve Nash", "Peja Stojaković"],
-        answer: "Allen Iverson",
+        answers: [
+            { choice: "Kobe Bryant", correct: false },
+            { choice: "Allen Iverson", correct: true },
+            { choice: "Steve Nash", correct: false },
+            { choice: "Peja Stojaković", correct: false },
+        ]
     },
     {
         question: "Which player scored 13 points in 33 seconds in an NBA game?",
-        choices: ["Kevin Durant", "Tracy McGrady", "Kobe Bryant", "Shaquille O'neal"],
-        answer: "Tracy McGrady",
+        answers: [
+            { choice: "Kevin Durant", correct: false },
+            { choice: "Tracy McGrady", correct: true },
+            { choice: "Kobe Bryant", correct: false },
+            { choice: "Shaquille O'neal", correct: false },
+        ]
     },
     {
-        question: "",
-        choices: [],
-        answer: "",
+        question: "Who invented the sport Basketball?",
+        answers: [
+            { choice: "Red Auerbach", correct: false },
+            { choice: "Dr. James Naismith", correct: true },
+            { choice: "Larry O'Brien", correct: false },
+            { choice: "David Stern", correct: false },
+        ]
     },
     {
-        question: "",
-        choices: [],
-        answer: "",
+        question: "Who scored the most points in a single NBA game?",
+        answers: [
+            { choice: "Wilt Chamberlain", correct: true },
+            { choice: "Bill Russell", correct: false },
+            { choice: "Kobe Bryant", correct: false },
+            { choice: "Luka Dončíć", correct: false },
+        ]
     },
     {
-        question: "",
-        choices: [],
-        answer: "",
+        question: "Which team has the most NBA championships?",
+        answers: [
+            { choice: "Los Angeles Lakers", correct: false },
+            { choice: "Golden State Warriors", correct: false },
+            { choice: "Boston Celtics", correct: true },
+            { choice: "Chicago Bulls", correct: false },
+        ]
     },
     {
-        question: "",
-        choices: [],
-        answer: "",
+        question: "Which team did Yao Ming played in his career?",
+        answers: [
+            { choice: "Cleveland Cavaliers", correct: false },
+            { choice: "Dallas Mavericks", correct: false },
+            { choice: "San Antonio Spurs", correct: false },
+            { choice: "Houston Rockets", correct: true },
+        ]
     },
     {
-        question: "",
-        choices: [],
-        answer: "",
+        question: "Which player won a three-peat championship from 2001-2003?",
+        answers: [
+            { choice: "Tim Duncan", correct: false },
+            { choice: "Kobe Bryant", correct: true },
+            { choice: "Dwyane Wade", correct: false },
+            { choice: "Allen Iverson", correct: false },
+        ]
     },
     {
-        question: "",
-        choices: [],
-        answer: "",
+        question: "Which player DID NOT play for the Boston Celtics?",
+        answers: [
+            { choice: "Larry Bird", correct: false },
+            { choice: "Bill Russell", correct: false },
+            { choice: "Jerry West", correct: true },
+            { choice: "Bob Cousey", correct: false },
+        ]
     },
     {
-        question: "",
-        choices: [],
-        answer: "",
+        question: "Which player DID NOT play for the Los Angeles Lakers?",
+        answers: [
+            { choice: "Magic Johnson", correct: false },
+            { choice: "Shaquille O'neal", correct: false },
+            { choice: "Luka Dončíć", correct: false },
+            { choice: "Paul Pierce", correct: true },
+        ]
     },
     {
-        question: "",
-        choices: [],
-        answer: "",
+        question: "Who has the all-time most 3-Pointers made?",
+        answers: [
+            { choice: "Stephen Curry", correct: true },
+            { choice: "Klay Thompson", correct: false },
+            { choice: "Ray Allen", correct: false },
+            { choice: "Reggie Miller", correct: false },
+        ]
     },
     {
-        question: "",
-        choices: [],
-        answer: "",
+        question: "Who is the all-time NBA career points leader?",
+        answers: [
+            { choice: "Michael Jordan", correct: false },
+            { choice: "Karl Malone", correct: false },
+            { choice: "Kareem Abdul-Jabbar", correct: false },
+            { choice: "LeBron James", correct: true },
+        ]
     },
     {
-        question: "",
-        choices: [],
-        answer: "",
+        question: "Which player played for the Serbia Men's Basketball Team?",
+        answers: [
+            { choice: "Luka Dončíć", correct: false },
+            { choice: "Dirk Nowitzki", correct: false },
+            { choice: "Toni Kukoč", correct: false },
+            { choice: "Nikola Jokić", correct: true },
+        ]
     },
 ]
